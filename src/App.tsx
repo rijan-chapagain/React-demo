15c8e4ddaf9e7e33126f5a04fcc2ae7a4c571d6a
rijan-chapagain <hrijon.chp@gmail.com>
2022-12-19 22:15:08 +0800

    Adds Fetch api call
    retrives data from .net api and log

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const weatherDataType = [
+  {
+    "date": "2022-12-19T13:21:13.256Z",
+    "temperatureC": 0,
+    "temperatureF": 0,
+    "summary": "string"
+  }
+]
+
 function App() {
+  useEffect(() => {
+    fetch("https://localhost:7124/WeatherForecast")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+        }
+      );
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
